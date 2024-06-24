26195878c4079bc24d2dde517d60e8c4a637aceb
vrnprkh <paintingburritos@gmail.com>
2024-06-24 03:13:34 -0400

    update gitignore

--- main.cc
+++ main.cc
@@ -3,11 +3,13 @@
 #include "rtweekend.h"
 #include "sphere.h"
 #include "vec3.h"
+#include <memory>
 
 int main() {
   hittable_list world;
   world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
   world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+  world.add(make_shared<sphere>(point3(0, 0, -0.5), 0.1));
 
   // Camera
   camera cam;
