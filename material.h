4995c8bd8c183d886e12944e266342c6a2ec8510
vrnprkh <paintingburritos@gmail.com>
2024-06-28 04:11:40 -0400

    dielectrics

--- material.h
+++ material.h
@@ -4,6 +4,7 @@
 #include "color.h"
 #include "hittable.h"
 #include "ray.h"
+#include "rtweekend.h"
 #include "vec3.h"
 class material {
 public:
@@ -38,14 +39,70 @@ private:
 
 class metal : public material {
 public:
-  metal(const color &albedo) : albedo{albedo} {}
+  metal(const color &albedo, double fuzz)
+      : albedo{albedo}, fuzz{fuzz < 1 ? fuzz : 1} {}
   bool scatter(const ray &r_in, const hit_record &rec, color &attenuation,
                ray &scattered) const override {
-    auto scattered_direction = reflect(r_in.direction(), rec.normal);
-    scattered = ray{rec.p, scattered_direction};
+    auto reflected = reflect(r_in.direction(), rec.normal);
+    reflected = unit_vector(reflected) + (fuzz * random_unit_vector());
+    scattered = ray{rec.p, reflected};
     attenuation = albedo;
     return true;
   };
+
+private:
+  color albedo;
+  double fuzz;
+};
+
+class dielectric : public material {
+public:
+  dielectric(double refraction_index) : refraction_index{refraction_index} {}
+
+  bool scatter(const ray &r_in, const hit_record &rec, color &attenuation,
+               ray &scattered) const override {
+
+    attenuation = color(0.95, 0.95, 0.95);
+    double ri = rec.front_face ? (1.0 / refraction_index) : refraction_index;
+    vec3 unit_dir = unit_vector(r_in.direction());
+
+    double cos_theta = fmin(dot(-unit_dir, rec.normal), 1.0);
+    double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+    bool cannot_refract = ri * sin_theta > 1.0;
+    vec3 direction;
+    if (cannot_refract || reflectance(cos_theta, ri) > random_double()) {
+      direction = reflect(unit_dir, rec.normal);
+    } else {
+      direction = refract(unit_dir, rec.normal, ri);
+    }
+
+    scattered = ray{rec.p, direction};
+    return true;
+  }
+
+private:
+  double refraction_index;
+  static double reflectance(double cosine, double refraction_index) {
+    // Use Schlick's approximation for reflectance.
+    auto r0 = (1 - refraction_index) / (1 + refraction_index);
+    r0 = r0 * r0;
+    return r0 + (1 - r0) * pow((1 - cosine), 5);
+  }
+};
+
+class weird : public material {
+public:
+  weird(const color &albedo) : albedo{albedo} {}
+
+  bool scatter(const ray &r_in, const hit_record &rec, color &attenuation,
+               ray &scattered) const override {
+    auto reflected = reflect(r_in.direction(), rec.normal);
+    reflected =
+        unit_vector(reflected) + 3 * vec3(unit_vector(reflected).x(), 0, 0);
+    scattered = ray{rec.p, reflected};
+    attenuation = albedo;
+    return true;
+  }
 
 private:
   color albedo;
